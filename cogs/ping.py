b288631817886aad9e9854c3ce21948dce971eee
Battery9 <abiswas9009@gmail.com>
2023-01-21 09:33:43 +0530

    Added some comments.

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,14 +1,17 @@
 import discord
 from discord.ext import commands
 
+#Making a class with command name.
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
+    #Writing my command function using this commands.command decorator from discord.py
     @commands.command()
     async def ping(self, ctx):
         latency = self.bot.latency * 1000
         await ctx.send(f'Pong! Latency is {latency:.2f} ms')
 
+#Adding my class as a cog to be loaded in main.py.
 async def setup(bot):
     await bot.add_cog(Ping(bot))
