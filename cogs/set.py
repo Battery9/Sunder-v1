e31228e1b5d368fd95b996adcbdae35ef7548e9a
Battery9 <abiswas9009@gmail.com>
2023-07-06 12:53:39 +0530

    Added Bot activity and status features

--- cogs/set.py
+++ cogs/set.py
@@ -25,6 +25,21 @@ class Set(commands.Cog):
             config.write(f)
         await ctx.send('Edited. Check help command.', delete_after=5)
 
+    @set.command()
+    async def status(self, ctx, *, text='online'):
+        config['var']['status'] = text
+        with open('res/data.ini', 'w') as f:
+            config.write(f)
+        await ctx.send("Done. Reboot the bot to display changes.")
+
+    @set.command()
+    async def activity(self, ctx, *, text='playing,something wrong'):
+        config['var']['activity'] = text
+        with open('res/data.ini', 'w') as f:
+            config.write(f)
+        await ctx.send("Done. Reboot the bot to display changes.")
+
+
 #Adding my class as a cog to be loaded in main.py.
 async def setup(bot):
     await bot.add_cog(Set(bot))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
+import configparser
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -14,11 +15,42 @@ intents.members = True
 bot = commands.Bot(command_prefix = '++', intents = intents)
 bot.remove_command('help')
 
+##Reading the data.ini file. Getting custom activity from their.
+config = configparser.ConfigParser()
+config.read('res/data.ini')
+activity = config["var"]["activity"]
+status = config['var']['status']
+
+##Config custom activity and status as per Discord util
+activity = activity.split(",")
+if activity[0] == 'playing':
+    activity=discord.Game(name=activity[1])
+elif activity[0] == 'streaming':
+    activity=discord.Streaming(name=activity[1], url=activity[2] if len(activity) > 2 else "http://cutt.ly/ure-dc")
+elif activity[0] == 'listening':
+    activity=discord.Activity(type=discord.ActivityType.listening, name=activity[1])
+else:
+    activity=discord.Activity(type=discord.ActivityType.watching, name=activity[1])
+
+if status == "dnd":
+    status = discord.Status.dnd
+elif status == 'idle':
+    status = discord.Status.idle
+else:
+    status = discord.Status.online
+
+
 #Prints a line on console after login as a bot.
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    ##Set Bot Activity
+    try:
+        await bot.change_presence(status=status, activity=activity)
+    except Exception as e:
+        print(e)
 
+##Basic Command error log
 async def on_command_error(ctx, error):
     print(f'Error: {error}')
 
