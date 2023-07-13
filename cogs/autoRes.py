37e979b2e6e8c761d8768cd662aca7ff8cc734f7
Battery9 <abiswas9009@gmail.com>
2023-07-13 11:06:04 +0530

    Added new responses

--- cogs/autoRes.py
+++ cogs/autoRes.py
@@ -36,7 +36,7 @@ class AutoRes(commands.Cog):
             @button(label="Yes!", style=ButtonStyle.green)
             async def yes(self, interaction: Interaction, button:Button):
                 await interaction.message.delete()
-                embed = Embed(title="**__❑ On-going Tournaments and Scrims__**", description="**>> Please visit the below channel to learn about and join different events happening right now across different platforms of UREsports. Our only talented admin have listed all the Scrims and Tournament just for you. Unleash your talents, show off your skills and roast your opponents with your squad.\nIt’s time to have some fun and make some noise**\n\n<#1125346374737727530>", color=0x00ff00).set_footer(text="Sunder | UREsports")
+                embed = Embed(title="**__❑ On-going Tournaments and Scrims__**", description="**>> Please visit the below channel to learn about and join different events happening right now across different platforms of UREsports. Our only talented admin have listed all the Scrims and Tournament just for you. Unleash your talents, show off your skills and roast your opponents with your squad.\nIt’s time to have some fun and make some noise**\n\n<#1125346374737727530>", color=0x00ff00).set_footer(text="Type !help for more | Sunder - UREsports")
 
                 ##Reading the ini file inside res to use.
                 config.read('res/data.ini')
@@ -53,13 +53,13 @@ class AutoRes(commands.Cog):
             if triger in message.content.lower() and not message.content.startswith(('+', '/', '!', '#', '@', '-', '.', '?')): #Add more elements into the startwith tuple to be ignored by the bot.
                 ctx = await self.bot.get_context(message)
                 help_command = self.bot.get_command('help')
-                embed = Embed(title=random.choice(help_res), color=0x00ff00)
+                embed = Embed(title=random.choice(help_res), description="Tap yes or call me with !help anytime", color=0x00ff00)
                 embed.set_thumbnail(url="https://i.ibb.co/LC96Nb1/ezgif-com-gif-maker.gif")
                 await message.channel.send(embed=embed, view=view(), delete_after=15)
 
         for trigger in scrims_triggers:
              if trigger in message.content.lower():
-                embed = Embed(title=random.choice(scrims_res), color=0x00ff00)
+                embed = Embed(title=random.choice(scrims_res), description="Tap yes or call me with !help anytime", color=0x00ff00)
                 embed.set_thumbnail(url="https://i.ibb.co/LC96Nb1/ezgif-com-gif-maker.gif")
                 await message.channel.send(embed=embed, view=view2(), delete_after=15)
     
